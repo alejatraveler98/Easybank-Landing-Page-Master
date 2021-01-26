afbc22c3761317c87be10dbb2aceccb30a730d8b
Alejandro Ramírez Gómez <alrg1720@gmail.com>
2021-01-26 16:03:52 -0600

    Fix en estados (hover) y fuentes en móvil

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 const menuToggle = document.getElementById('menu-nav');
 let   mediaQuery = window.matchMedia("(min-width:1000px)");
 let   mainNav    = document.getElementById('main-nav')
+let   mainLink   = document.querySelector('.main-nav__link');
+
+
 menuToggle.addEventListener('click', ()=> {
     menu()
 })
@@ -10,17 +13,8 @@ mediaQuery.addEventListener('change', (e)=>{
 })
 
 const menu = () => {
-    // document.body.style.overflowY ="hidden";
     document.body.classList.toggle('overflow-hidden')
     mainNav.classList.toggle('display-block');
     mainNav.classList.toggle('display-none');
 }
 
-const media=(e) =>{
- if(e.matches){
-    // document.body.style.overflowY ="scroll";
- }
-}
-// function menu() {
-//     document.body.style.overflowY ="hidden"
-// }
